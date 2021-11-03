9d280cc89f48aae66e61dccdfb95f4a3a1959d14
megann <megann.elizabeth.king@gmail.com>
2021-11-03 18:16:43 -0400

    Add methods to get user choice and display results

--- lib/steam_charts/cli.rb
+++ lib/steam_charts/cli.rb
@@ -19,15 +19,17 @@ class SteamCharts::CLI
 
     def get_user_choice
         user_choice = gets.strip.to_i
-        if valid_choice(user_choice, @search_options)
-            puts "valid"
-        else
-            puts "invalid"
-        end
+        display_games_for(user_choice) if valid_choice(user_choice, @search_options)
     end
 
     def valid_choice(input, data)
         input <= data.length && input > 0
+    end
+
+    def display_games_for(user_choice)
+        choice = @search_options[user_choice - 1]
+        puts "Here are the current results for #{choice}:\n"
+        puts "games"
     end
 
 end
