150d0115ef46c765065ae6477a6e3fbdd7495643
megann <megann.elizabeth.king@gmail.com>
2021-11-03 17:12:14 -0400

    Started CLI class, added menu options

--- lib/steam_charts/cli.rb
+++ lib/steam_charts/cli.rb
@@ -1,6 +1,20 @@
 class SteamCharts::CLI
     def call
         puts "Welcome to Steam Charts!"
-        binding.pry
+        get_search_options
+        get_user_selection
+        # get_games_from_user_selection
+        # list_games
     end
+
+    def get_search_options
+        @search_options = ["Search by Trending Games", "Search by Top 10 Games", "Search by Top Records", "Search by All Top Games"]
+    end
+
+    def get_user_selection
+        @search_options.each.with_index(1) do |option, index |
+            puts "#{index}. #{option}"
+        end
+    end
+
 end
